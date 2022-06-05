c3b619ec7b68341e8ae4554dee5050f1c2664961
VincentCohen <v.cohen90@gmail.com>
2022-06-05 23:18:26 +0200

    Added memcached support

--- src/index.php
+++ src/index.php
@@ -1,2 +1,14 @@
 <?php
+
+$memcached = new Memcached();
+$memcached->addServer("memcached", 11211);
+$r = $memcached->get("test");
+
+if ($r) {
+    echo $r;
+} else {
+    echo "No result. Refresh..";
+    $r->set("test", "Test! Test! Test!") or die("Couldn't save anything to memcached...");
+}
+
 phpinfo();
